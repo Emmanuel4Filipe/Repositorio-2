ab9ed65d92a480b272045e77a9b988dad1ef86d6
Emmanuel4Filipe <emmanuel.correa@aluno.senai.br>
2026-03-31 19:51:47 -0300

    Commit 1

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '..', 'view'));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    titulo: 'Sistema Teste',
+    mensagem: 'Aplicacao rodando',
+    data: new Date().toLocaleString('pt-BR')
+  });
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Servidor local rodando em http://localhost:${port}`);
+  });
+}
+
+module.exports = app;
